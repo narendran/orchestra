13a8824bad2cb682230ecb36f00df05552dc428d
Narendran Thangarajan <narendran.thangarajan@gmail.com>
2014-11-08 23:37:31 -0800

    Modified translator code

--- component_translate.py
+++ component_translate.py
@@ -23,10 +23,13 @@ def expand_component(jsontext):
     output_python.append('import redis')
     output_python.append('import json')
     output_python.append("r = redis.StrictRedis(host='localhost', db=0)")
+    output_python.append("s = redis.StrictRedis(port=6380, host='localhost', db=0)")
     output_python.append("p = r.pubsub(ignore_subscribe_messages=True)")
+    output_python.append("sources = []")
 
     for source in component['sources']:
         output_python.append("p.subscribe('%s')" % (source))
+        output_python.append("sources.append('%s')" % (source))
 
     # Add component filtering
 
@@ -35,30 +38,38 @@ def expand_component(jsontext):
     for line in module['logic']['initialization']:
         output_python.append(line)
 
+    # Handling constants/threshold mentioned in the component's spec
+    output_python.append("constants = []")
+    output_python.append("field = []")
+    for line in component['module']['params']:
+        if not ":" in line:
+            output_python.append("constants.append('%s')" % (line))
+        else:
+            output_python.append("field.append('"+line.split(':')[1]+"')")
+    output_python.append("params = [0] * (len(sources) + len(constants))")
+
     # From this point on, we handle processing every new tuple
     output_python.append("")
     output_python.append("for m in p.listen():")
-
     # Ignore non-"message" messages
     output_python.append("  if m is None or m['type'] not in ('message', 'pmessage'):")
     output_python.append("    continue")
     output_python.append("")
-    # output_python.append("  print m")
     output_python.append("  data = json.loads(m['data'])")
+    output_python.append("  p_index = sources.index(str(m['channel']))")
+    output_python.append("  params[p_index] = float(data[field[p_index]])")
 
-    # Set up parameters for module iteration
-    output_python.append("  params = []")
-    i = 0
-    for line in component['module']['params']:
-        output_python.append("  params.append(float(data['%s']))" % (line))
-        i = i + 1
-
+    output_python.append("  for i,line in enumerate(constants):")
+    output_python.append("    params[len(field)+i] = float(line)")
+    # Check if all the params are set, and only then proceed to module
+    output_python.append("  if all(p is not None for p in params):")
     for line in module['logic']['iteration']:
-        output_python.append("  " + line)
+        output_python.append("    " + line)
 
     for line in module['logic']['output']:
-        output_python.append("  r.publish('" + component['publish'] + "', " + line + ')')
-        output_python.append("  print '" + component['publish'] + "', " + line + '')
+        output_python.append("    s.publish('" + component['publish'] + "', " + line + ')')
+        output_python.append("    print '" + component['publish'] + "', " + line + '')
+    output_python.append("    params = [None] * (len(sources) + len(constants))")
 
     return '\n'.join(output_python)
 
